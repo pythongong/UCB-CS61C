e90617f254ef6cfb6d6c29ae0a1003c79b4c1333
pythongong <alex@DESKTOP-786CP1C>
2024-06-24 18:01:33 +0800

    lab2

--- labs/lab02/bork.c
+++ labs/lab02/bork.c
@@ -7,7 +7,9 @@
 #include <string.h>
 
 char *alloc_str(int len) {
-    return malloc(len*sizeof(char));
+    char* data = malloc((len + 1)*sizeof(char));
+    data[len] = '\0';
+    return data;
 }
 
 /* Str helper functions */
@@ -68,9 +70,9 @@ int main(int argc, char*argv[]) {
         Str bork_substr = translate_to_bork(src_str.data[i]);
         dest_str = concat(dest_str, bork_substr);
     }
-
     printf("Input string: \"%s\"\n", src_str.data);
     printf("Length of translated string: %d\n", dest_str.len);
     printf("Translate to Bork: \"%s\"\n", dest_str.data);
+    free_Str(dest_str);
     return 0;
 }
