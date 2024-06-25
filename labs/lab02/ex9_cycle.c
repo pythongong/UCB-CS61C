b7c6a4f5f3ccc7bae33f7034096242b9a9d52919
pythongong <alex@DESKTOP-786CP1C>
2024-06-25 08:53:52 +0800

    lab2

--- labs/lab02/ex9_cycle.c
+++ labs/lab02/ex9_cycle.c
@@ -2,5 +2,22 @@
 #include "ex9_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    node *slow_pointer;
+    node *fast_pointer;
+    slow_pointer = head;
+    fast_pointer = head;
+    while (slow_pointer != NULL && fast_pointer != NULL) {
+        slow_pointer = slow_pointer->next;
+        if (fast_pointer->next == NULL) {
+            return 0;
+        }
+        fast_pointer = fast_pointer->next->next;
+        if (slow_pointer == fast_pointer)
+        {
+            return 1;
+        }
+        
+    }
+    return 0;
+    
 }
