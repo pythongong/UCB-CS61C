3601cef132f3382d23f98ba717aed42161a74631
pythongong <alex@DESKTOP-786CP1C>
2024-07-18 15:01:17 +0800

    proj3 custom test

--- projects/proj3/tests/integration-custom/in/cutom.s
+++ projects/proj3/tests/integration-custom/in/cutom.s
@@ -10,3 +10,14 @@ srai t0 s1 1
 srai t0 s1 0
 slti t0 s1 1
 slti t0 s1 0
+
+# R type
+add t1 t0 s1 
+and t1 t0 s1
+or t1 t0 s1
+xor t1 t0 s1
+sll t1 t0 s1
+srl t1 t0 s1
+sra t1 t0 s1
+slt t1 t0 s1
+
